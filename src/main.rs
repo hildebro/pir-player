be5c4039624d4995a697302618a52274bf31cbf0
Alexander Hildebrandt <alex@hilde.dev>
2022-07-16 12:57:46 +0200

    create third loop option where the sensor isn't probed at all

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,63 @@
-use gpio::sysfs::SysFsGpioInput;
+use std::io::Write;
+use std::{env, fs, io, process::Command, process::Stdio, thread, time};
+
 use gpio::{GpioIn, GpioValue};
 use rand::{seq::SliceRandom, thread_rng};
-use std::{env, fs, io, process::Command, process::Stdio, thread, time};
-use std::io::Write;
 
 fn main() {
-    // Opening the occupied GPIO pin.
-    let mut gpio_input = gpio::sysfs::SysFsGpioInput::open(4).unwrap();
-
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 && args[1] == "--debug" {
-        debug_loop(&mut gpio_input)
-    } else {
-        player_loop(&mut gpio_input)
+
+    if args.len() <= 1 {
+        player_loop()
+    }
+
+    let option = args[1].as_str();
+    match option {
+        "-q" => quiet_loop(),
+        "-d" => dev_loop(),
+        _ => {
+            eprintln!("Unexpected option: {}", option);
+            std::process::exit(exitcode::IOERR);
+        }
     }
 }
 
-/// Just a simple loop to check, whether the PIR sensor is giving expected output.
-fn debug_loop(gpio_input: &mut SysFsGpioInput) -> ! {
+/// Loop for testing the music player functionality. Instead of probing the sensor, just play songs
+/// one after the other.
+fn dev_loop() -> ! {
+    let mut song_paths: Vec<String> = get_song_paths();
+
+    loop {
+        // Get the next song to play
+        let current_song = match song_paths.pop() {
+            None => {
+                song_paths = get_song_paths();
+                song_paths.pop().unwrap()
+            }
+            Some(song) => song,
+        };
+
+        // Play the current song and wait until it's finished.
+        println!("Now playing: {}", current_song);
+        Command::new("cvlc")
+            .arg(current_song)
+            .arg("vlc://quit")
+            .stdout(Stdio::null())
+            .status()
+            .expect("failed to execute process");
+        println!("Song finished, waiting for motion...");
+    }
+}
+
+/// Just a simple loop to check, whether the PIR sensor is giving expected output. Instead of
+/// playing a song on movement, the program prints `!` in the terminal.
+fn quiet_loop() -> ! {
+    // Opening the occupied GPIO pin.
+    let mut gpio_input = gpio::sysfs::SysFsGpioInput::open(4).unwrap();
     loop {
         match gpio_input.read_value().unwrap() {
             GpioValue::Low => print!("."),
-            GpioValue::High => print!("!")
+            GpioValue::High => print!("!"),
         }
         // Must flush in order for print! to show up immediately.
         io::stdout().flush().unwrap();
@@ -29,7 +65,11 @@ fn debug_loop(gpio_input: &mut SysFsGpioInput) -> ! {
     }
 }
 
-fn player_loop(gpio_input: &mut SysFsGpioInput) -> ! {
+/// Loop that awaits movement and then plays a song. Afterwards, return to awaiting movement.
+fn player_loop() -> ! {
+    // Opening the occupied GPIO pin.
+    let mut gpio_input = gpio::sysfs::SysFsGpioInput::open(4).unwrap();
+
     let mut song_paths: Vec<String> = get_song_paths();
 
     println!("Starting loop...");
